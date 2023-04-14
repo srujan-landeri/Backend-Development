d5461dd4e568a1bc4216d4fa8de70cfef8f9b9e3
Landeri Srujan <srujanlanderi@gmail.com>
2023-04-14 17:22:49 +0530

    Uploaded

---
+++ Node js/2 - Globals.js
@@ -0,0 +1,5 @@
+// Accessing global variables
+
+console.log('Current directory name: ' + __dirname)
+console.log("Current File Name " + __filename)
+console.log("Current running process's id : " + process.pid )
