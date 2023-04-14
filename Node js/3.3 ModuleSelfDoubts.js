e98a9bff7dc82720423a9105f333594c1646f251
Landeri Srujan <srujanlanderi@gmail.com>
2023-04-14 18:06:40 +0530

    Uploaded

---
+++ Node js/3.3 ModuleSelfDoubts.js
@@ -0,0 +1,8 @@
+module.exports.name = "Tony"
+module.exports.school = 'Andrews School'
+
+console.log(module)
+
+// can not access the two variables as follows
+// console.log(name)
+// console.log(school)
