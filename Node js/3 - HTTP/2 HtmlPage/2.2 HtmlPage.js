b3b31c24a2d080c1b46bd7c80fdc5b052cddefa8
Landeri Srujan <srujanlanderi@gmail.com>
2023-04-15 18:43:42 +0530

    Uploaded

--- Node js/3 - HTTP/2 HtmlPage/2.2 HtmlPage.js
+++ Node js/3 - HTTP/2 HtmlPage/2.2 HtmlPage.js
@@ -2,12 +2,18 @@ const http = require('http')
 const {readFileSync} = require('fs')
 
 const HomePage = readFileSync(__dirname + '/2.0 index.html')
+const HomeStyle = readFileSync(__dirname + '/style.css')
 
 const server = http.createServer((req,res) => {
-    res.writeHead(200, {'content-type':'text/html'})
-
+    
     if(req.url === '/'){
+        res.writeHead(200, {'content-type':'text/html'})
         res.write(HomePage)
+    }
+
+    if(req.url === '/style.css'){
+        res.writeHead(200, {'content-type':'text/css'})
+        res.write(HomeStyle)
     }
 
     res.end()
